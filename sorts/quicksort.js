a1c0b62464c700bb7317fab4bcb0a5949784e574
Artur Basak <artur.basak.devingrodno@gmail.com>
2025-02-17 17:31:29 +0300

    merge sort

--- sorts/quicksort.js
+++ sorts/quicksort.js
@@ -1,25 +1,56 @@
-// Проходим все элементы и на каждой итерации ищем минимальное значение
-// если нашли меньше, чем текущее, то меняем местами и т.д.
-// O(n^2)
-function selectionSort (arr) {
-    for (let i = 0; i < arr.length; i++) {
-        let min = i;
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[min] > arr[j]) {
-                min = j;
-            }
-        }
+// Выбираем опорный элемент и делим массив на 2 части,
+// первая часть - это элементы меньше опорного, вторая - больше опорного
+// повторяем рекурсивно для обеих частей
+// объединяем результаты
 
-        if (min !== i) {
-            const tmp = arr[i];
-            arr[i] = arr[min];
-            arr[min] = tmp;
+// лучший случай O(n*log(n))
+// O(n^2)
+function quickSort (arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const pivot = arr[arr.length - 1];
+    const left = [];
+    const right = [];
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
         }
     }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+// Не создаем массивы для правой и левой части, пробегаем эти куски и сортируем элементы прямо в текущем
+function quickSortWithoutExtraMemory(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        const pivot = partition(arr, left, right);
+        quickSortWithoutExtraMemory(arr, left, pivot - 1);
+        quickSortWithoutExtraMemory(arr, pivot + 1, right);
+    }
+
     return arr;
+}
+
+function partition (arr, left, right) {
+    const pivot = arr[arr.length - 1];
+    let i = left - 1;
+
+    for (let j = left; j < right; i++) {
+        if (arr[i] < pivot) {
+            i++;
+            const temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
 }
 
 
 const list = JSON.parse(process.argv[2]);
-console.log(selectionSort(list).join(','));
+console.log(quickSort(list).join(','));
 
